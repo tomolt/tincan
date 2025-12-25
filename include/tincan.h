287f0d83c2ddec59f8927c120cd308386ee5e10d
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2025-12-25 18:31:24 +0100

    Generating code docu with doxygen

--- include/tincan.h
+++ include/tincan.h
@@ -12,30 +12,51 @@
 #include <stddef.h>
 #include <float.h>
 
-/* === Customizable Allocators === :alloc: */
+/**
+ * \defgroup allocator Customizable Allocators
+ * @{
+ */
 
+/// Customizable allocator.
 typedef struct {
 	void *userPointer;
 	void *(*alloc)(void *userPointer);
 	void (*free)(void *userPointer, void *memoryPointer);
 } Tin_Allocator;
 
-/* === Vector Math === :vec: */
+/** @} */
 
+/**
+ * \defgroup vector Vector Math
+ * @{
+ */
+
+/// An epsilon value for the Tin_Scalar type.
 #define TIN_EPSILON FLT_EPSILON
+
+/// The floating point type used to represent real-valued scalars.
 typedef float Tin_Scalar;
 
-struct Tin_Vec3 { Tin_Scalar c[3]; };
-typedef struct Tin_Vec3 Tin_Vec3;
+/// Represents a real-valued (floating point) 3D vector.
+typedef struct Tin_Vec3 {
+	/// The cardinal axes components of the vector.
+	Tin_Scalar c[3];
+} Tin_Vec3;
 
 #define TIN_VEC3(x, y, z) (Tin_Vec3){{x,y,z}}
 
+/// Invert the direction of a 3D vector.
 Tin_Vec3 tin_neg_v3  (Tin_Vec3 x);
+/// Add two 3D vectors together.
 Tin_Vec3 tin_add_v3  (Tin_Vec3 a, Tin_Vec3 b);
+/// Subtract a 3D vector from another.
 Tin_Vec3 tin_sub_v3  (Tin_Vec3 a, Tin_Vec3 b);
+/// Scale a 3D vector by a scalar value.
 Tin_Vec3 tin_scale_v3(Tin_Scalar a, Tin_Vec3 x);
 Tin_Vec3 tin_saxpy_v3(Tin_Scalar a, Tin_Vec3 x, Tin_Vec3 y);
+/// Calculate the cross product of two 3D vectors.
 Tin_Vec3 tin_cross_v3(Tin_Vec3 a, Tin_Vec3 b);
+/// Calculate the dot product of two 3D vectors.
 Tin_Scalar tin_dot_v3(Tin_Vec3 a, Tin_Vec3 b);
 Tin_Scalar tin_length_v3(Tin_Vec3 v);
 Tin_Vec3 tin_normalize_v3(Tin_Vec3 v);
@@ -49,7 +70,12 @@ Tin_Vec3 tin_m3_times_v3(const Tin_Scalar matrix[3*3], Tin_Vec3 vector);
 Tin_Vec3   tin_gram_schmidt(Tin_Vec3 fixed, Tin_Vec3 var);
 Tin_Scalar tin_prlgram_area(Tin_Vec3 e1, Tin_Vec3 e2);
 
-/* === Transforms === :trf: */
+/** @} */
+
+/**
+ * \defgroup transform Transforms
+ * @{
+ */
 
 typedef struct {
 	Tin_Scalar rotation[3*3];
@@ -62,7 +88,12 @@ Tin_Vec3 tin_fwtrf_dir  (const Tin_Transform *transform, Tin_Vec3 vec);
 Tin_Vec3 tin_bwtrf_point(const Tin_Transform *transform, Tin_Vec3 vec);
 Tin_Vec3 tin_bwtrf_dir  (const Tin_Transform *transform, Tin_Vec3 vec);
 
-/* === Polytopes === :poly: */
+/** @} */
+
+/**
+ * \defgroup polytope Polytopes
+ * @{
+ */
 
 typedef struct {
 	Tin_Vec3  *vertices;
@@ -75,7 +106,12 @@ typedef struct {
 
 Tin_Vec3 tin_polytope_support(const void *geometry, Tin_Vec3 dir);
 
-/* === Shapes === :shape: */
+/** @} */
+
+/**
+ * \defgroup shape Collision Shapes
+ * @{
+ */
 
 #define TIN_SPHERE   's'
 #define TIN_POLYTOPE 'p'
@@ -91,19 +127,38 @@ typedef struct {
 
 void tin_shape_aabb(const Tin_Shape *shape, const Tin_Transform *transform, Tin_Vec3 *aabbMin, Tin_Vec3 *aabbMax);
 
-/* === Rigid Bodies === :body: */
+/** @} */
 
+/**
+ * \defgroup body Rigid Bodies
+ * @{
+ */
+
+/// A numerical ID identifying a rigid body within a scene.
 typedef uint32_t Tin_BodyID;
 
-typedef struct Tin_Body Tin_Body;
-struct Tin_Body {
+/// A rigid body.
+typedef struct Tin_Body {
+	/// The collision shape.
 	const Tin_Shape *shape;
+	/// The position, orientation, and scale in space.
 	Tin_Transform transform;
 	
+	/// The linear velocity.
 	Tin_Vec3      velocity;
+	/**
+	 * \brief The bodies' angular velocity.
+	 *
+	 * The direction of the vector represents the rotation axis,
+	 * the magnitude represents the speed of the rotation in
+	 * radians per second.
+	 */
 	Tin_Vec3      angularVelocity;
 	
+	/// One over the mass.
 	Tin_Scalar    invMass;
+	/// The inverse of the inertia tensor along the cardinal axes,
+	/// assuming that the center of mass is at the origin of the rigid body.
 	Tin_Scalar    invInertia[3*3];
 	
 	Tin_BodyID    islandID;
@@ -112,10 +167,16 @@ struct Tin_Body {
 	
 	Tin_Vec3      aabbMin;
 	Tin_Vec3      aabbMax;
-};
+} Tin_Body;
 
-/* === Minkowski Sum === :mink: */
-/* (difference of transformed convex polytopes) */
+/** @} */
+
+/**
+ * \defgroup minkowski Minkowski Sum
+ * @{
+ */
+
+/// Implicit Minkowski Sum (difference) of two transformed polytopes.
 typedef struct {
 	const Tin_Polytope  *polytope1;
 	const Tin_Transform *transform1;
@@ -127,6 +188,7 @@ Tin_Vec3 tin_polysum_support(const void *geometry, Tin_Vec3 dir);
 
 /* === Minkowski Portal Refinement === :mpr: */
 
+/// A ray in 3D space.
 typedef struct {
 	Tin_Vec3 origin;
 	Tin_Vec3 dir;
@@ -144,7 +206,12 @@ int tin_construct_portal(const void *geometry, Tin_SupportFunc support,
 void tin_refine_portal  (const void *geometry, Tin_SupportFunc support,
 	const Tin_Ray *r, Tin_Portal *p);
 
-/* === Contact Points === :contact: */
+/** @} */
+
+/**
+ * \defgroup contact Contact Points
+ * @{
+ */
 
 #define TIN_MAX_CONTACTS 4
 
@@ -202,7 +269,12 @@ typedef struct Tin_Scene Tin_Scene;
 
 void tin_arbiter_prestep(Tin_Scene *scene, Tin_Arbiter *arbiter, Tin_Scalar (*velocities)[6], Tin_Scalar invDt);
 
-/* === Pair-Indexed Hashtable === :pair: */
+/** @} */
+
+/**
+ * \defgroup pairtable Pair-Indexed Hashtable
+ * @{
+ */
 
 typedef struct {
 	size_t   elemLow;
@@ -230,7 +302,12 @@ bool tin_find_pair(const Tin_PairTable *table, size_t elemA, size_t elemB, void 
 void tin_insert_pair(Tin_PairTable *table, size_t elemA, size_t elemB, void *payload);
 void tin_delete_pair(Tin_PairTable *table, size_t elemA, size_t elemB);
 
-/* === Island Detection & Freezing === :island: */
+/** @} */
+
+/**
+ * \defgroup island Island Detection & Freezing
+ * @{
+ */
 
 typedef struct Tin_Scene Tin_Scene;
 
@@ -238,7 +315,12 @@ Tin_BodyID tin_island_find(Tin_Scene *scene, Tin_BodyID bodyID);
 void tin_island_union(Tin_Scene *scene, Tin_BodyID bodyID1, Tin_BodyID bodyID2);
 void tin_build_islands(Tin_Scene *scene);
 
-/* === Scenes / Worlds === :scene: */
+/** @} */
+
+/**
+ * \defgroup scene Scenes
+ * @{
+ */
 
 typedef struct Tin_SweepPrune Tin_SweepPrune;
 
@@ -266,10 +348,32 @@ void tin_scene_step(Tin_Scene *scene, Tin_Scalar (*velocities)[6]);
 void tin_integrate(Tin_Scene *scene, Tin_Scalar dt);
 void tin_simulate(Tin_Scene *scene, Tin_Scalar dt, double (*gettime)(), double timings[6]);
 
+/**
+ * \brief Create a new rigid body.
+ *
+ * \param scene The scene to which the rigid body is added.
+ * \param shape The shape of the rigid body.
+ *              This pointer must remain valid for as long as the rigid body exists.
+ *              It is not modified or freed by tincan.
+ * \param invMass One over the mass of the rigid body. A value of zero means infinite mass.
+ * \return A numerical ID identifying the body within the scene.
+ */
 Tin_BodyID tin_add_body(Tin_Scene *scene, const Tin_Shape *shape, Tin_Scalar invMass);
+
+/**
+ * \brief Delete a rigid body.
+ *
+ * \param scene  The scene from which the rigid body is to be deleted.
+ * \param bodyID A numerical ID identifying the body within the scene.
+ */
 void       tin_delete_body(Tin_Scene *scene, Tin_BodyID bodyID);
 
-/* === Broadphase === :broad: */
+/** @} */
+
+/**
+ * \defgroup broadphase Broadphase Collision Detection
+ * @{
+ */
 
 #define TIN_BLOOM_NUM_BITS (1024 * 8)
 
@@ -289,6 +393,7 @@ typedef struct {
 	Tin_SweepEvent *events;
 } Tin_SweepAxis;
 
+/// Incremental Sweep-and-Prune broadphase collision detection.
 typedef struct Tin_SweepPrune {
 	Tin_Scene *scene;
 	Tin_SweepAxis axes[3];
@@ -303,5 +408,7 @@ void tin_sweep_prune_axis(Tin_SweepPrune *sap, int axisIdx, const unsigned *bloo
 	Tin_BodyID **collidersOut, size_t *numCollidersOut);
 void tin_sweep_prune(Tin_SweepPrune *sap,
 	Tin_BodyID **collidersOut, size_t *numCollidersOut);
+
+/** @} */
 
 #endif
